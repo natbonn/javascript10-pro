6b9bef5f4a90e2cbbd6183b839f75ab8c6329e5b
natbonn <natbon@hotmail.gr>
2026-07-29 21:06:48 +0300

    Add number conversion examples and circle area calculation

---
+++ ch1-numbers/circle.js
@@ -0,0 +1,6 @@
+// Εμβαδόν ενός κύκλου με PI * r^2 (radius εις το τετρ.)
+const radius = 5;
+
+const area = Math.PI * radius ** 2;
+
+console.log(`Circle with radius ${radius}cm has area ${area.toFixed(2)}cm\u00B2`)
--- ch1-numbers/number-constructor.js
+++ ch1-numbers/number-constructor.js
@@ -13,10 +13,23 @@ console.log("Positive div by zero: " + (-12 / 0));  // -Infinity
 
 // Explicit Conversion
 let num = Number("12");     // num -> 12
+
 if(Number.isInteger(num)) {
-    console.log("num is integer: " + Number.isInteger(num))
+    console.log("num is integer: " + Number.isInteger(num));
 } else {
-    console.log("Not an integer")
+    console.log("Not an integer");
 }
 
-//
+// Implicit Convercion --- Coercion
+let aNum = 4 / "aueb";       // μετατρέπει το string σε αριθμητικό
+
+// Υπάρχει και global(έξω από wrapper) isNaN("Hello")  true  // δεν συνίσταται - κυρίως για backward compatibility
+// Number.isNaN("Hello")  false  - συνίσταται
+if(Number.isNaN(aNum)) {
+    console.log("aNum is NaN? " + aNum);
+} else {
+    console.log(aNum);
+}
+
+// Format decimals
+console.log((12.12345).toFixed(2));    // toFixed(2) επιστρέφει string -- 12.12

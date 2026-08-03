5ae2a43abe2eca2a485660b96300c46ca8988eec
natbonn <natbon@hotmail.gr>
2026-08-03 15:27:46 +0300

    Fix syntax error in string traversal loop and ensure proper formatting

--- ch3-strings/traverse.js
+++ ch3-strings/traverse.js
@@ -1,5 +1,9 @@
 const coding = 'Coding';
 
 for (let i = o; i < coding.length; i++) {
-    console.log(coding[i] + " ")
+    console.log(coding[i] + " ");
+}
+
+for (const char of coding) {
+    console.log(char);
 }

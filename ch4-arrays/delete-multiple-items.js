672c172c13bdf8070492cee8edbea0d1b3807a67
natbonn <natbon@hotmail.gr>
2026-08-05 11:23:21 +0300

    Add deleteBackwards function to remove specified values from an array

---
+++ ch4-arrays/delete-multiple-items.js
@@ -0,0 +1,13 @@
+const arr = [ 2, 2, 3, 2, 5, 2 ];
+
+const deleteBackwards = (arr, val) => {
+
+    if (!Array.isArray(arr)) return;
+
+    // Ελέγχει από το τέλος προς την αρχή
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === val) {
+            arr.splice(i, 1)
+        }
+    }
+}

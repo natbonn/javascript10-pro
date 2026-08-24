bb48bc8d6165169d3df1642b28600deac4a24fe4
natbonn <natbon@hotmail.gr>
2026-08-24 20:19:06 +0300

    Add palindrome functions to check for palindromic strings

--- ch3-strings/review.js
+++ ch3-strings/review.js
@@ -22,3 +22,26 @@ function reverse2(str) {
 
 let str2 = 'Coding';
 console.log(reverse2(str2));
+
+// Άσκηση 2
+// Μία συνάρτηση που να ελέγχει αν ένα string είναι παλυνδρομικό. 
+// Αγνοήστε πεζά και κεφαλαία
+// Επιστρέφει boolean 
+// Σωστοί και οι 2 τρόποι επίλυσης
+
+function palindrome(str) {
+    const cleaned = str.trim().toUpperCase().split(/\s+/).join('');
+
+    for (let i = 0, j = cleaned.length - 1; i < j; i++, j--) {
+        if (cleaned[i] !== cleaned[j]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function palindrome2(str) {
+    const reversed = str.trim().toUpperCase().split(/\s+/).reverse().join('');
+    return reversed === str.trim().toUpperCase().split().koin('');
+}
+ 

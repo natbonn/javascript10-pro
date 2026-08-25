5c9b62f97cc9d4f0d45a10fcc38cb8de45cda5d2
natbonn <natbon@hotmail.gr>
2026-08-25 18:36:25 +0300

    Add functions to calculate average, check array equality, and remove occurrences from an array

--- ch4-arrays/review.js
+++ ch4-arrays/review.js
@@ -18,3 +18,61 @@ function findMax(arr) {
 }
 
 console.log(findMax([13, 5, 78, 42]));
+
+// Άσκηση 2
+// Μία συνάρτηση που λαμβάνει ως είσοδο έναν πίνακα αριθμών
+// και επιστρέφει τον μέσο όρο
+
+function findAvg(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    for (const num of arr) {
+        sum =+ num;
+    }
+
+    return sum / arr.length;
+}
+
+// Functional Solution
+function findAvg2(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        return 0;
+    } 
+
+    return arr.reduce((total, num) => total + num, 0) / arr.length;
+}
+
+// Άσκηση 3
+// Μία συνάρτηση που ελέγχει (επιστρέφει true/false)
+// αν δύο πίνακες είναι ίσοι: ίδιο μήκος και ίδια στοιχεία
+
+function equalArrays(arr1, arr2) {
+    if (!Array.isArray(arr1) || arr1.length === 0) {
+        return 0;
+    }
+
+    if (!Array.isArray(arr2) || arr2.length === 0) {
+        return 0;
+    }
+
+    if (arr1.length !== arr2.length) return false;
+    
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+console.log(equalArrays([1, 2, 3, 4], [1, 2, 3, 4]));
+
+// Άσκηση 4
+// Μία συνάρτηση που λαμβάνει ως είσοδο έναν πίνακα 
+// και μία τιμή και επιστρέφει έναν νέο πίνακα
+// χωρίς να εμφανίζει την τιμή αυτή
+
+function removeAllOccurances(arr, valueToRemove) {
+    return arr.filter(value => value !== valueToRemove);
+}

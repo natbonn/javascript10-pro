fcd0738435e2acb70b3a341984eb93a11a3ee8ac
natbonn <natbon@hotmail.gr>
2026-08-28 19:06:18 +0300

    Update comments for function syntax and add examples for optional and default parameters

--- ch6-functions/functions.js
+++ ch6-functions/functions.js
@@ -1,4 +1,4 @@
-// 1. Basic Function Syntax
+// 1. Basic Function Syntax - κάνει hoisting
 function sayHello(name) {
     return `Hello ${name}`
 }
@@ -16,3 +16,21 @@ console.log(add(3, 8))
 const multiply = (a, b) => a * b;
 
 console.log(multiply(4, 6))
+
+// Optional Params - old way
+function createButton(text, color) {
+    color = color || 'blue'
+    console.log(text, color)
+}
+
+createButton('Hello')             // Clear
+createButton('Hello', '')         // θα είναι blue όχι clear
+
+// Default Params  --  ES6
+// Λειτουργούν ως overload
+function createButton2(text, color = 'blue') {
+    console.log(text, color)
+}  
+ createButton2('Hello')            // Hello blue
+ createButton2('Hello', 'red')     // Hello red
+ createButton2('Hello', '')        // Hello

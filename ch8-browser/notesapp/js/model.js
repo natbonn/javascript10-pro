06d902d8bb59d45fe2d260c0ae985749cefd22aa
natbonn <natbon@hotmail.gr>
2026-09-22 21:06:46 +0300

    Add initial implementation of notes management with getNotes, insertNote, and strikeThrough functions

---
+++ ch8-browser/notesapp/js/model.js
@@ -0,0 +1,19 @@
+let notes = [];   // note = {key: id, note: noteTxt, softDeleted: false} πολλά objects
+let id = 1;       // και τα 2 = state
+
+export function getNotes() {
+    return notes;
+}
+
+export function insertNote(noteTxt) {
+    notes = [...notes, {key: id, note: noteTxt, softDeleted: false}];
+    id++;
+}
+
+export function strikeThrough(key) {
+    notes = notes.map((noteObj) => 
+        noteObj.key === key 
+        ? { ...noteObj, softDeleted: !noteObj.softDeleted }
+        : noteObj,
+    );
+}

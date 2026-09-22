d1da7b5f90e1d3944b6100a26dc616a1c502c203
natbonn <natbon@hotmail.gr>
2026-09-22 19:22:30 +0300

    Add color styling for counter display based on value

--- ch8-browser/counter/view.js
+++ ch8-browser/counter/view.js
@@ -2,4 +2,12 @@ const counterElement = document.getElementById('counter')
 
 export function render(counter) {
     counterElement.textContent = counter;
+    styleCounter(counter);
+}
+
+function styleCounter(counter) {
+    counterElement.classList.toggle('color-positive', counter > 0);
+    counterElement.classList.toggle('color-negative', counter < 0);
+    counterElement.classList.toggle('color-zero', counter === 0);
+
 }
